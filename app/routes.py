0b0a46c2698134ef1df590ee0b5688bc8e59f952
rootcanopy <dave.trees201@gmail.com>
2020-04-16 22:44:50 +0200

    havent even got going yet and already debugging

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/base')
 def base():
     user = {'username': 'Davie'}
-    return  
+    return  render_template('base.html', title='Home', user=user)
