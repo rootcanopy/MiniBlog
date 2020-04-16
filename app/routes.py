2d8765f5e9a623d8bd8c39ff5fd7d495ecd312db
rootcanopy <dave.trees201@gmail.com>
2020-04-17 00:53:16 +0200

    login form done now

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,25 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
-@app.route('/base')
 def base():
-    user = {'username': 'Davie'}
-    posts= [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return  render_template('base.html', title='Home', user=user)
+    return render_template('base.html')
+
+
+@app.route('/')
+def home():
+    render_template('index.html', title='Home')
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return  render_template('login.html', title='Log In', form=form)
+
+
+if __name__ == '__main__':
+            app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
--- mini.py
+++
@@ -1,2 +0,0 @@
-sieve = [True] * 101
-for i in range(2, 100):
