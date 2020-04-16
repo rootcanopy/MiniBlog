b845664e1cc7f6ddb8c76debb56d9006f3dd2a14
rootcanopy <dave.trees201@gmail.com>
2020-04-17 00:13:04 +0200

    env variables set up and hashed

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
+import env
 from flask import Flask
+
 
 app = Flask(__name__)
 
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 # @ BOTTOM BECAUSE OF CIRCULAR IMPORTS
 from app import routes
---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+  
+os.environ.get('SECRET_KEY') = '8bf1555c499fe3cc55021fd1e87585e5'
+
+os.environ["PORT"] = "5000"
