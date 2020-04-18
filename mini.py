664d91f32f6d864eb32570ed3fd5f08fb1d69e0e
rootcanopy <dave.trees201@gmail.com>
2020-04-18 12:16:02 +0200

    everything is good here :)

--- mini.py
+++ mini.py
@@ -1,22 +1,24 @@
 import os
 import env
-from flask_login import LoginManager, current_user, login_user
+
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_mongoengine import MongoEngine
+from flask_login import login_user
 from forms import LoginForm
 from werkzeug.security import generate_password_hash, check_password_hash
+#from models import User
 
 
 app = Flask(__name__)
 
 
 # SSSSHHHH
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["MONGO_DBNAME"] = "blog"
 
 db = MongoEngine(app)
-#login_manager = LoginManager(app)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+#login_manager = LoginManager()
 #login_manager.init_app(app)
 
 
@@ -33,16 +35,17 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     #IF SESSION ACTIVE IN BROWSER
-    #if current_user.is_authenticated:
-        #return redirect(url_for('index'))
+    if current_user.is_authenticated == True:
+        return redirect(url_for('index'))
     form = LoginForm()
 
     # IF ITS RIGHT THE FIRST TIME
     if form.validate_on_submit():
-        user = User.mongo.db.find_one(username=form.username.data).first_or_404()
+        existing_u = User.objects(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+
             flash(f'Invalid username or password')
-            return redirect(url_for('home'))
+            return redirect(url_for('login'))
             #flash('Login success for user {}, remember_me{}'.format(
             #form.username.data, form.remember_me.data))
         login_user(user, remember=form.remember_me.data)
--- models.py
+++ models.py
@@ -1,14 +1,7 @@
 from datetime import datetime
-from app import db
 from flask_mongoengine.wtf import model_form, Document, Post
-from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-
-"""
-@login_manager.user_loader
-def load_user(_id):
-    return User.mongo.db.find(toInt(_id))
-"""
+from flask_login import UserMixin
 
 class User(UserMixin, db.Document):
     _id = db.IntField (primary_key=True)
@@ -22,10 +15,17 @@ class User(UserMixin, db.Document):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
+    # ENCRYPTING USER PASSWORDS
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(pk=user_id)first()
 
 
 class Post(db.Document):
